adae74f2c705448469cbfa24fbbc5ad6f97e0161
Tom Holmes <tholmes59@gmail.com>
2019-03-19 22:58:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Tom"
+  end
+  
+  get '/hometown' do
+    "My hometown is San Francisco"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is Free Fallin' by Tom Petty"
+  end 
 end
